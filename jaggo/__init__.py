e908d4b547983d4a9f4fc2310ab74a7c2c95d5f0
mfrancis95 <mikefrancis95@gmail.com>
2018-07-22 17:44:05 -0400

    Improve text formatting

--- jaggo/__init__.py
+++ jaggo/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
 from datetime import date
+from flask import Flask, render_template
 from os import environ
 
 _start_date = date.fromordinal(int(environ['START_DATE']))
@@ -8,4 +8,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', days = (date.today() - _start_date).days, text = environ['TEXT'])
+    days = (date.today() - _start_date).days
+    return render_template('index.html', text = environ['TEXT'].format(days, 'day' if days == 1 else 'days'))
